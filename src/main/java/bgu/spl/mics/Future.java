ade7426c765a7611330933a97207ff624d8c14b6
Tal Kadosh <talkad@post.bgu.ac.il>
2019-12-10 23:30:22 +0200

    implemented Future.class as a thread safe object

--- src/main/java/bgu/spl/mics/Future.java
+++ src/main/java/bgu/spl/mics/Future.java
@@ -1,14 +1,13 @@
 package bgu.spl.mics;
 
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * A Future object represents a promised result - an object that will
  * eventually be resolved to hold a result of some operation. The class allows
  * Retrieving the result once it is available.
- * 
+ *
  * Only private methods may be added to this class.
  * No public constructor is allowed except for the empty constructor.
  *
@@ -17,89 +16,89 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Future<T> {
 
 	private T result;
-	private volatile boolean isDone;
-	
+	private AtomicBoolean isDone;
+
 
 	/**
 	 * This should be the the only public constructor in this class.
 	 */
 	public Future() {
 		result=null;
-		isDone=false;
+		isDone=new AtomicBoolean(false);
 	}
 
 	/**
-     * retrieves the result the Future object holds if it has been resolved.
-     * This is a blocking method! It waits for the computation in case it has
-     * not been completed.
-     * <p>
-     *
+	 * retrieves the result the Future object holds if it has been resolved.
+	 * This is a blocking method! It waits for the computation in case it has
+	 * not been completed.
+	 * <p>
+	 *
 	 * @pre none
 	 * @post isDone()==true
 	 * @return return the result of type T if it is available, if not wait until it is available.
-     */
+	 */
 
-	public synchronized T  get() {
-			while (!isDone) {
-				/**
-				 *  result.wait()
-				 */
-			}	
+	public T  get() {
+		synchronized (result) {
+			while (!isDone.get()) {
+				try {
+					result.wait();
+				} catch (InterruptedException ignored) { }
+			}
+			result.notifyAll();
+		}
 		return result;
 	}
-	
+
 	/**
-     * Resolves the result of this Future object.
+	 * Resolves the result of this Future object.
 	 *
 	 * @pre  T!=null
 	 * @post get()=={@param result}
-     */
-	public void resolve (T result) {
-		//TODO : implement this correctly
+	 */
+	public synchronized void resolve (T result) {
 		if(result!=null) {
-			this.result = result; //maybe i should clone it- so it will be safe thread
-			isResolved = true;
+			this.result = result;
+			isDone.set(true);
 		}
 	}
-	
+
 	/**
 	 * @pre none
 	 * @post trivial
-     * @return true if this object has been resolved, false otherwise
-     */
+	 * @return true if this object has been resolved, false otherwise
+	 */
 	public boolean isDone() {
-		return isResolved;
-	}
-	
-	/**
-     * retrieves the result the Future object holds if it has been resolved,
-     * This method is non-blocking, it has a limited amount of time determined
-     * by {@code timeout}
-     * <p>
-     * @param timeout 	the maximal amount of time units to wait for the result.
-     * @param unit		the {@link TimeUnit} time units to wait.
-	 * @pre none
-	 * @post  {@param timeout}>={@code timeout} && isDone()==true
-	 * @post  {@param timeout}<{@code timeout} && isDone()==false
-     * @return return the result of type T if it is available, if not, 
-     * 	       wait for {@code timeout} TimeUnits {@code unit}. If time has
-     *         elapsed, return null.
-     */
-	public T get(long timeout, TimeUnit unit) {
-		Lock lock=new ReentrantLock();
-		try {
-			if (lock.tryLock(timeout, unit)) {
-				if(!isDone())
-					resolve(result);
-				return result;
-			}
-		} catch (InterruptedException e) {
-			//do nothing
-		} finally {
-			lock.unlock();
-		}
-		return null;
+		return isDone.get();
 	}
 
-	//await function
+	/**
+	 * retrieves the result the Future object holds if it has been resolved,
+	 * This method is non-blocking, it has a limited amount of time determined
+	 * by {@code timeout}
+	 * <p>
+	 * @param timeout 	the maximal amount of time units to wait for the result.
+	 * @param unit		the {@link TimeUnit} time units to wait.
+	 * @pre {@param unit}!=null
+	 * @post  {@param timeout}>={@code timeout} && isDone()==true
+	 * @post  {@param timeout}<{@code timeout} && isDone()==false
+	 * @return return the result of type T if it is available, if not,
+	 * 	       wait for {@code timeout} TimeUnits {@code unit}. If time has
+	 *         elapsed, return null.
+	 */
+	public T get(long timeout, TimeUnit unit) {
+		synchronized(result) {
+			if (unit != null) {
+				TimeUnit milliTime=TimeUnit.MILLISECONDS;
+				while (!isDone()) {
+					try {
+						result.wait(milliTime.convert(timeout,unit)); //waiting for {@param timeout} milliseconds
+					} catch (InterruptedException ignored) { }
+				}
+				result.notifyAll();
+			}
+		}
+		return result;
+	}
+
 }
